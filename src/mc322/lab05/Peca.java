3d91d2e2719eb21636ce7d1c02a0c5c5f752a1ff
Wallace Lima <wallacegsantoslima@gmail.com>
2021-04-24 23:14:29 -0300

    Cria método de apresentação

--- src/mc322/lab05/Peca.java
+++ src/mc322/lab05/Peca.java
@@ -1,15 +1,36 @@
 package mc322.lab05;
 
 public class Peca {
-	char jogador;    // 'B' : Jogador das brancas ; 'P' : Jogador das Pretas
-	char tipoPeca;  // '1' : Peao ; '2' : Dama
-	int posicao[] = new int[2];
-	Movimento movimento = new Movimento();
+	private char jogador;    // 'B' : Jogador das brancas ; 'P' : Jogador das Pretas
+	private char tipoPeca;  // '1' : Peao ; '2' : Dama
+	private int posicao[] = new int[2];
+	// private Movimento movimento = new Movimento();
 	
-	Peca(char player, char tipoPeca, int posX, int posY){
-		this.player = player;
+	Peca(char jogador, char tipoPeca, int posX, int posY){
+		this.jogador = jogador;
 		this.tipoPeca = tipoPeca;
 		this.posicao[0] = posX;
 		this.posicao[1] = posY;
+	}
+
+	public String toString(){
+		if (jogador == 'B'){
+			if (tipoPeca == '1'){
+				return "b";
+			}
+			else
+			{
+				return "B";
+			}
+		}
+		else{
+			if (tipoPeca == '1'){
+				return "p";
+			}
+			else{
+				return "P";
+			}
+		}
+
 	}
 }
