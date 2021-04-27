0abb438f727fada39bc0056d91b44c440137e11c
Wallace Lima <wallacegsantoslima@gmail.com>
2021-04-27 15:22:19 -0300

    Prototipo da Classe

--- src/mc322/lab05/Peca.java
+++ src/mc322/lab05/Peca.java
@@ -2,16 +2,12 @@ package mc322.lab05;
 
 public class Peca {
 	private char jogador;    // 'B' : Jogador das brancas ; 'P' : Jogador das Pretas
-	private char tipoPeca;  // '1' : Peao ; '2' : Dama
 	private int posicao[] = new int[2];
 	private Tabuleiro tab; // Tabuleiro ao qual a peça pertence.
-	private Movimento movimento;
 	
-	Peca(char jogador, char tipoPeca, int i, int j, Tabuleiro tab){
+	Peca(char jogador, int i, int j, Tabuleiro tab){
 		this.jogador = jogador;
-		this.tipoPeca = tipoPeca;
 		this.tab = tab;
-		this.movimento = new Movimento();
 		setPosicao(i, j);
 	}
 
@@ -26,29 +22,15 @@ public class Peca {
 		this.posicao[1] = j;
 	}
 
+	// Retorna se o movimento pode ser realizado e seleciona a peça que foi capturada
+	// durante o processo
 	public boolean ehMovimentoValido(int iAlvo, int jAlvo)
 	{
 		return true;
 	}
 
-	public String toString(){
-		if (jogador == 'B'){
-			if (tipoPeca == '1'){
-				return "b";
-			}
-			else
-			{
-				return "B";
-			}
-		}
-		else{
-			if (tipoPeca == '1'){
-				return "p";
-			}
-			else{
-				return "P";
-			}
-		}
-
+	public String toString()
+	{	
+		return "X"; // Peça genérica;
 	}
 }
