2d09de15c34fcad3e78013f2bc231705736657e2
Wallace Lima <wallacegsantoslima@gmail.com>
2021-04-27 15:27:38 -0300

    Prototipo da subclasse

--- src/mc322/lab05/Peao.java
+++ src/mc322/lab05/Peao.java
@@ -1,13 +1,20 @@
 package mc322.lab05;
 
-public class Peao extends Peca{
-	Peao(char jogador, char tipoPeca, int posX, int posY){
-		super(jogador, tipoPeca, posX, posY);
+public class Peao extends Peca
+{
+	Peao(char jogador, int i, int j, Tabuleiro tab)
+	{
+		super(jogador, i, j, tab);
 	}
 	
-	public boolean ehMovimentoValidoPeao() {
-		//
+	public boolean ehMovimentoValidoPeao(int i, int j)
+	{
+		// Deve setar a peça que será comida caso o movimento seja válido; (essa conexão é mesmo boa?)
+		return true;
 	}
 	
-	// Falta a fun��o de pegar a dire�ao : Se for peao branco, as dire��es s�o (-1,1) - (1,1) , se for preto (-1,-1) - (1,-1).
+	public String toString()
+	{
+		return (jogador == 'B') ? "b" : "p";
+	}
 }
