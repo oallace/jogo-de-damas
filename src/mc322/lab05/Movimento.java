35e5ce46451505480f62dad962ccc2ff28d9e2f1
Igor-HBS <70449472+Igor-HBS@users.noreply.github.com>
2021-04-29 14:26:46 -0300

    Update Movimento.java

--- src/mc322/lab05/Movimento.java
+++ src/mc322/lab05/Movimento.java
@@ -2,13 +2,16 @@ package mc322.lab05;
 
 public class Movimento {
 	// Analisa se peça de uma dada posição é inimiga.
-	public boolean ehInimigo() {
-		//
+	public boolean ehInimigo(Peca peca) {
+		if (peca.getJogador() != peca.getTabuleiro().getJogadorAtual()) {
+			return true;
+		}
+		return false;
 	}
 	
 	
-	// Dado uma dire��o (no caso do pe�o e dama, dire��es diagonais : (1,1) - (1,-1) - (-1,1) - (-1,-1), analisa as poss�veis posi��es
-	// at� chegar no limite de movimentos (no caso da Dama, apenas quando chegar na borda do tabuleiro) ou encontrar um aliado ou
+	// Dado uma direção (no caso do peão e dama, direções diagonais : (1,1) - (1,-1) - (-1,1) - (-1,-1), analisa as possíveis posições
+	// até chegar no limite de movimentos (no caso da Dama, apenas quando chegar na borda do tabuleiro) ou encontrar um aliado ou
 	// inimigo.
 	public boolean untilBlockedPath() {
 		//
