801464c985d1b5d299a56729d4d609d5653922fd
Wallace Lima <wallacegsantoslima@gmail.com>
2021-05-05 10:28:43 -0300

    atualização

--- src/mc322/lab05/AppDama.java
+++ src/mc322/lab05/AppDama.java
@@ -1,7 +1,44 @@
 package mc322.lab05;
 
 public class AppDama {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        String resultado[] = executaJogo("../testes/teste01.csv");
+    }
+    
+    public static String[] executaJogo(String entrada)
+    {
+        CSVHandling csv;
+        String comandos[];
+        String resultado[];
+        Tabuleiro tab;
+        int iInicio, jInicio, iFim, jFim;
         
+        csv = new CSVHandling();
+        csv.setDataSource(entrada); // setar por argumentos de entrada
+        comandos = csv.requestCommands();
+        resultado = new String[comandos.length];
+        tab = new Tabuleiro(); // setar jogador inicial;
+        System.out.println("Início:");
+        tab.imprimirTabuleiro();
+        for (int i = 0; i < comandos.length; i++)
+        {
+            // converte o comando em string em seus valores inteiros:
+            System.out.printf("Source: %s\n", comandos[i].substring(0,2));
+            jInicio = Character.getNumericValue(comandos[i].charAt(0)) - Character.getNumericValue('a');
+            iInicio = Character.getNumericValue(comandos[i].charAt(1)) - Character.getNumericValue('1');
+            System.out.printf("Target: %s\n", comandos[i].substring(3,5));
+            jFim = Character.getNumericValue(comandos[i].charAt(3)) - Character.getNumericValue('a');
+            iFim = Character.getNumericValue(comandos[i].charAt(4)) - Character.getNumericValue('1');
+            // executa o movimento:
+            System.out.println(iInicio);
+            System.out.println(jInicio);
+            System.out.println(iFim);
+            System.out.println(jFim);
+            tab.solicitaMovimento(iInicio, jInicio, iFim, jFim);
+            tab.imprimirTabuleiro();
+            resultado[i] = tab.toString();
+        }
+        
+        return resultado;
     }
 }
--- src/mc322/lab05/Tabuleiro.java
+++ src/mc322/lab05/Tabuleiro.java
@@ -1,83 +1,72 @@
 package mc322.lab05;
 
 public class Tabuleiro {
-	private Peca pecas[][];      // Guarda as peças no tabuleiro
-	private int numPecasBrancas;
-	private int numPecasPretas;
-	private char jogadorAtual;      // 'B' : Brancas ; 'P' : Pretas
-	private Peca pecaCapturada;     // Peça que foi capturada em um dado turno
+    private Peca pecas[][]; // Guarda as peças no tabuleiro
+    private int numPecasBrancas;
+    private int numPecasPretas;
+    private char jogadorAtual; // 'B' : Brancas ; 'P' : Pretas
+    private Peca pecaCapturada; // Peça que foi capturada em um dado turno
 
-	/* Descreve a forma inicial do tabuleiro:
-    1: espaço pode ser ocupado por uma peça
-    0: espaço inválido
-    */
-    private static int formaTabuleiro[][] = {
-        {0, 1, 0, 1, 0, 1, 0, 1},
-        {1, 0, 1, 0, 1, 0, 1, 0},
-        {0, 1, 0, 1, 0, 1, 0, 1},
-        {1, 0, 1, 0, 1, 0, 1, 0},
-        {0, 1, 0, 1, 0, 1, 0, 1},
-        {1, 0, 1, 0, 1, 0, 1, 0},
-        {0, 1, 0, 1, 0, 1, 0, 1},
-        {1, 0, 1, 0, 1, 0, 1, 0},
-    };
-    
-    
+    /*
+     * Descreve a forma inicial do tabuleiro: 1: espaço pode ser ocupado por uma
+     * peça 0: espaço inválido
+     */
+    private static int formaTabuleiro[][] = { { 0, 1, 0, 1, 0, 1, 0, 1 }, { 1, 0, 1, 0, 1, 0, 1, 0 },
+            { 0, 1, 0, 1, 0, 1, 0, 1 }, { 1, 0, 1, 0, 1, 0, 1, 0 }, { 0, 1, 0, 1, 0, 1, 0, 1 },
+            { 1, 0, 1, 0, 1, 0, 1, 0 }, { 0, 1, 0, 1, 0, 1, 0, 1 }, { 1, 0, 1, 0, 1, 0, 1, 0 }, };
+
     // Inicia um novo tabuleiro completo
-    Tabuleiro(){
-    	pecas = new Peca[8][8];
-    	numPecasBrancas = 12;
-    	numPecasPretas = 12;
-    	pecaCapturada = null;
-    	jogadorAtual = '0';  // Inicialmente ainda não se sabe o jogador inicial.
-    	
-    	for (int i = 0; i < 8; i++) {
-    		for (int j = 0; j < 8; j++) {
-    			if (ehEspacoValido(i, j)) {
+    Tabuleiro() {
+        pecas = new Peca[8][8];
+        numPecasBrancas = 12;
+        numPecasPretas = 12;
+        pecaCapturada = null;
+        jogadorAtual = 'B'; // Inicialmente ainda não se sabe o jogador inicial.
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (ehEspacoValido(i, j)) {
                     if (i < 3) {
                         pecas[i][j] = new Peao('P', i, j, this);
-                    }
-                    else if (i > 4){
+                    } else if (i > 4) {
                         pecas[i][j] = new Peao('B', i, j, this);
-                    }
-                    else {
+                    } else {
                         pecas[i][j] = null;
                     }
-    			}
-                else{
+                } else {
                     pecas[i][j] = null;
                 }
             }
-    	}
+        }
     }
-    
+
     public Peca getPeca(int i, int j) {
-    	if (ehEspacoValido(i, j) && !ehEspacoVazio(i, j)) {
-    		return this.pecas[i][j];
-    	}
-    	return null;
+        if (ehEspacoValido(i, j) && !ehEspacoVazio(i, j)) {
+            return this.pecas[i][j];
+        }
+        return null;
     }
-    
+
     public char getJogadorAtual() {
-    	return this.jogadorAtual;
+        return this.jogadorAtual;
     }
-    
+
     public void setJogadorAtual(char jogador) {
-    	this.jogadorAtual = jogador;
+        this.jogadorAtual = jogador;
     }
 
     public Peca getPecaCapturada() {
-    	return this.pecaCapturada;
+        return this.pecaCapturada;
     }
-    
-    public void setPecaCapturada(Peca p){
+
+    public void setPecaCapturada(Peca p) {
         this.pecaCapturada = p;
     }
-    
+
     // Checa se o espaço informado pode ser ocupado por uma peça
-    public boolean ehEspacoValido(int i, int j){
-        if (i >= 0 && i < 8 && j >= 0 && j < 8){
-            if (formaTabuleiro[i][j] == 1){
+    public boolean ehEspacoValido(int i, int j) {
+        if (i >= 0 && i < 8 && j >= 0 && j < 8) {
+            if (formaTabuleiro[i][j] == 1) {
                 return true;
             }
         }
@@ -85,49 +74,37 @@ public class Tabuleiro {
     }
 
     // retorna se o espaço informado está vazio
-    public boolean ehEspacoVazio(int i, int j)
-    {
-        if (ehEspacoValido(i, j))
-        {
-            if (pecas[i][j] == null)
-            {
+    public boolean ehEspacoVazio(int i, int j) {
+        if (ehEspacoValido(i, j)) {
+            if (pecas[i][j] == null) {
                 return true;
             }
         }
         return false;
     }
-    
-    
-    // Altera o  jogador do movimento atual
-    public void mudarTurno()
-    {
-    	this.jogadorAtual = (this.jogadorAtual == 'B') ? 'P' : 'B'; 
+
+    // Altera o jogador do movimento atual
+    public void mudarTurno() {
+        this.jogadorAtual = (this.jogadorAtual == 'B') ? 'P' : 'B';
     }
-    
-    
+
     // Promover um peão para uma dama caso necessário.
     public void promoverPeca(Peca peca) {
-    	if (peca instanceof Peao) {
-    		int posicaoPeca[] = peca.getPosicao();
-			char jogador = peca.getJogador();
-			
-    		if ((posicaoPeca[0] == 0 && jogador == 'B') || (posicaoPeca[0] == 7 && jogador == 'P')) {
-    			this.pecas[posicaoPeca[0]][posicaoPeca[1]] = new Dama(jogador, posicaoPeca[0], posicaoPeca[1], this);
-    		}
-    	}
-    }
-    
-    
-    private void capturarPeca()
-    {
-        if (pecaCapturada != null)
-        {
-            if (pecaCapturada.getJogador() == 'B')
-            {
-                this.numPecasBrancas--;
+        if (peca instanceof Peao) {
+            int posicaoPeca[] = peca.getPosicao();
+            char jogador = peca.getJogador();
+
+            if ((posicaoPeca[0] == 0 && jogador == 'B') || (posicaoPeca[0] == 7 && jogador == 'P')) {
+                this.pecas[posicaoPeca[0]][posicaoPeca[1]] = new Dama(jogador, posicaoPeca[0], posicaoPeca[1], this);
             }
-            else
-            {
+        }
+    }
+
+    private void capturarPeca() {
+        if (pecaCapturada != null) {
+            if (pecaCapturada.getJogador() == 'B') {
+                this.numPecasBrancas--;
+            } else {
                 this.numPecasPretas--;
             }
             int posicaoPeca[] = pecaCapturada.getPosicao();
@@ -135,81 +112,74 @@ public class Tabuleiro {
             setPecaCapturada(null);
         }
     }
-    
-    
+
     // Realiza o movimento indicado, quando possível. Caso contrário, não faz nada.
-    public void solicitaMovimento(int iInicio, int jInicio, int iFim, int jFim)
-    {
-    	if (ehEspacoValido(iInicio, jInicio) && !ehEspacoVazio(iInicio, jInicio) && pecas[iInicio][jInicio].getJogador() == this.jogadorAtual
-    		&& ehEspacoValido(iFim, jFim) && ehEspacoVazio(iFim, jFim))
-        {
+    public void solicitaMovimento(int iInicio, int jInicio, int iFim, int jFim) {
+        if (ehEspacoValido(iInicio, jInicio) && !ehEspacoVazio(iInicio, jInicio)
+                && pecas[iInicio][jInicio].getJogador() == this.jogadorAtual && ehEspacoValido(iFim, jFim)
+                && ehEspacoVazio(iFim, jFim)) {
             Peca pecaSelecionada = pecas[iInicio][jInicio];
 
-            if (pecaSelecionada.ehMovimentoValido(iFim, jFim))
-            {
-            	
-            	// Mudar turno se necessario
-            	if (this.pecaCapturada != null && pecaSelecionada.ehPossivelComer()) {
-            		mudarTurno();
-            	}
-            	
+            if (pecaSelecionada.ehMovimentoValido(iFim, jFim)) {
+
+                // Mudar turno se necessario
+                if (this.pecaCapturada != null && pecaSelecionada.ehPossivelComer()) {
+                    mudarTurno();
+                }
+
                 // capturar Peca:
                 capturarPeca();
-                
+
                 // atualiza as coordenadas de inicio e fim:
                 pecaSelecionada.setPosicao(iFim, jFim);
                 pecas[iFim][jFim] = pecaSelecionada;
                 pecas[iInicio][jInicio] = null;
-                
+
                 // Promove a peça caso seja necessário
                 promoverPeca(pecaSelecionada);
             }
         }
     }
-    
-    
- // Retorna uma string que representa a linha
-    private String stringLinha(int i){
+
+    // Retorna uma string que representa a linha
+    private String stringLinha(int i) {
         String res = "";
 
-        for(int j = 0; j < 8; j++){
-        	if(pecas[i][j] != null){
-        		res += pecas[i][j];
-            }
-            else{
-            	res += '-';
+        for (int j = 0; j < 8; j++) {
+            if (pecas[i][j] != null) {
+                res += pecas[i][j];
+            } else {
+                res += '-';
             }
         }
 
         return res + "\n";
     }
-    
-    
-    public void imprimirTabuleiro(){
+
+    public void imprimirTabuleiro() {
         String linhaAtual;
 
-        for(int i = 0; i < 8; i++){
+        for (int i = 0; i < 8; i++) {
             System.out.print(i + 1);
             linhaAtual = stringLinha(i);
-            for (int k = 0; k < linhaAtual.length(); k++){
+            for (int k = 0; k < linhaAtual.length(); k++) {
                 System.out.print(" " + linhaAtual.charAt(k));
             }
         }
         System.out.println("  a b c d e f g h\n");
     }
-    
-    public String toString(){
+
+    public String toString() {
         String res = "";
 
-        for(int i = 0; i < 8; i++){
+        for (int i = 0; i < 8; i++) {
             res += stringLinha(i);
         }
 
         return res;
     }
 
-    public void exportarArquivo()
-    {
+    public void exportarArquivo() {
 
     }
 }
