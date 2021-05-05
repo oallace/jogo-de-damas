739ecfa973c6d6f41e167cf44eb59c92b5016090
Igor-HBS <70449472+Igor-HBS@users.noreply.github.com>
2021-05-05 13:19:11 -0300

    Update Peao.java

--- src/mc322/lab05/Peao.java
+++ src/mc322/lab05/Peao.java
@@ -16,31 +16,21 @@ public class Peao extends Peca
 		}
 		
 		if (this.jogador == 'P' && direcaoDiagonal[0] == -1) {
-			if (this.getTabuleiro().ehEspacoValido(this.posicao[0]-2, this.posicao[1]+2) && this.getTabuleiro().ehEspacoVazio(this.posicao[0]-2, this.posicao[1]+2)) {
-				if (ehMovimentoValido(this.posicao[0]-2, this.posicao[1]+2)) {
-					return true;
-				}
+			if (ehPossivelComerDirecao(-2, +2)) {
+				return true;
 			}
-			
-			else if (this.getTabuleiro().ehEspacoValido(this.posicao[0]-2, this.posicao[1]-2) && this.getTabuleiro().ehEspacoVazio(this.posicao[0]-2, this.posicao[1]-2)) {
-				if (ehMovimentoValido(this.posicao[0]-2, this.posicao[1]-2)) {
-					return true;
-				}
+			else if (ehPossivelComerDirecao(-2, -2)) {
+				return true;
 			}
 			
 			return false;
 		}
 		else if (this.jogador == 'B' && direcaoDiagonal[0] == 1) {
-			if (this.getTabuleiro().ehEspacoValido(this.posicao[0]+2, this.posicao[1]+2) && this.getTabuleiro().ehEspacoVazio(this.posicao[0]+2, this.posicao[1]+2)) {
-				if (ehMovimentoValido(this.posicao[0]+2, this.posicao[1]+2)) {
-					return true;
-				}
+			if (ehPossivelComerDirecao(2, 2)) {
+				return true;
 			}
-			
-			else if (this.getTabuleiro().ehEspacoValido(this.posicao[0]+2, this.posicao[1]-2) && this.getTabuleiro().ehEspacoVazio(this.posicao[0]+2, this.posicao[1]-2)) {
-				if (ehMovimentoValido(this.posicao[0]+2, this.posicao[1]-2)) {
-					return true;
-				}
+			else if (ehPossivelComerDirecao(2, -2)) {
+				return true;
 			}
 			
 			return false;
@@ -50,33 +40,31 @@ public class Peao extends Peca
 		return this.movimento.untilBlockedPath(this, direcaoDiagonal, iAlvo, jAlvo, 1);
 	}
 	
+	public boolean ehPossivelComerDirecao(int di, int dj) {
+		if (this.getTabuleiro().ehEspacoValido(this.posicao[0]+di, this.posicao[1]+dj) && this.getTabuleiro().ehEspacoVazio(this.posicao[0]+di, this.posicao[1]+dj)) {
+			int direcaoDiagonal[] = new int[2];
+			direcaoDiagonal[0] = di/Math.abs(di);
+			direcaoDiagonal[1] = dj/Math.abs(dj);
+			
+			if (this.movimento.untilBlockedPath(this, direcaoDiagonal, this.posicao[0]+di, this.posicao[1]+dj, 1)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	public boolean ehPossivelComer() {
-		if (this.getTabuleiro().ehEspacoValido(this.posicao[0]+2, this.posicao[1]+2) && this.getTabuleiro().ehEspacoVazio(this.posicao[0]+2, this.posicao[1]+2)) {
-			if (ehMovimentoValido(this.posicao[0]+2, this.posicao[1]+2)) {
-				this.getTabuleiro().setPecaCapturada(null);
-				return true;
-			}
+		if (ehPossivelComerDirecao(2, 2)) {
+			return true;
 		}
-		
-		else if (this.getTabuleiro().ehEspacoValido(this.posicao[0]+2, this.posicao[1]-2) && this.getTabuleiro().ehEspacoVazio(this.posicao[0]+2, this.posicao[1]-2)) {
-			if (ehMovimentoValido(this.posicao[0]+2, this.posicao[1]-2)) {
-				this.getTabuleiro().setPecaCapturada(null);
-				return true;
-			}
+		else if (ehPossivelComerDirecao(2, -2)) {
+			return true;
 		}
-		
-		else if (this.getTabuleiro().ehEspacoValido(this.posicao[0]-2, this.posicao[1]+2) && this.getTabuleiro().ehEspacoVazio(this.posicao[0]-2, this.posicao[1]+2)) {
-			if (ehMovimentoValido(this.posicao[0]-2, this.posicao[1]+2)) {
-				this.getTabuleiro().setPecaCapturada(null);
-				return true;
-			}
+		else if (ehPossivelComerDirecao(-2, 2)) {
+			return true;
 		}
-		
-		else if (this.getTabuleiro().ehEspacoValido(this.posicao[0]-2, this.posicao[1]-2) && this.getTabuleiro().ehEspacoVazio(this.posicao[0]-2, this.posicao[1]-2)) {
-			if (ehMovimentoValido(this.posicao[0]-2, this.posicao[1]-2)) {
-				this.getTabuleiro().setPecaCapturada(null);
-				return true;
-			}
+		else if (ehPossivelComerDirecao(-2, -2)) {
+			return true;
 		}
 		
 		return false;
