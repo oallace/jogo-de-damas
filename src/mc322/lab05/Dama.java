421f8856974af6fb8395d1ecdbe0638d39747d92
Igor-HBS <70449472+Igor-HBS@users.noreply.github.com>
2021-05-05 14:18:43 -0300

    Update Dama.java

--- src/mc322/lab05/Dama.java
+++ src/mc322/lab05/Dama.java
@@ -18,41 +18,6 @@ public class Dama extends Peca
 	}
 	
 	
-	public boolean ehPossivelComerDirecao(int di, int dj) {
-		int numPecasInimigas = 0;
-		int posicaoAtual[] = new int[2];
-		int direcaoDiagonal[] = new int[2];
-		direcaoDiagonal[0] = di;
-		direcaoDiagonal[1] = dj;
-		posicaoAtual[0] = this.getPosicao()[0] + di;
-		posicaoAtual[1] = this.getPosicao()[1] + dj;
-		
-		while (this.getTabuleiro().ehEspacoValido(posicaoAtual[0], posicaoAtual[1])) {
-			if (this.getTabuleiro().ehEspacoVazio(posicaoAtual[0], posicaoAtual[1])) {
-				if (this.movimento.untilBlockedPath(this, direcaoDiagonal, posicaoAtual[0], posicaoAtual[1], 7) && this.getTabuleiro().getPecaCapturada() != null) {
-					return true;
-				}
-			}
-			else {
-				Peca pecaAtual = this.getTabuleiro().getPeca(posicaoAtual[0], posicaoAtual[1]);
-				if (!this.movimento.ehInimigo(this, pecaAtual)) {
-					break;
-				}
-				else {
-					numPecasInimigas += 1;
-					if (numPecasInimigas > 1) {
-						break;
-					}
-				}
-			}
-			posicaoAtual[0] += di;
-			posicaoAtual[1] += dj;
-		}
-		
-		return false;
-	}
-	
-	
 	public String toString()
 	{
 		return (jogador == 'B') ? "B" : "P";
